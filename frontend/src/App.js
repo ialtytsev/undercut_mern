899101e56937128d65d21cecc05e1b3f56e01626
ialtytsev <alty1998alty@gmail.com>
2022-08-05 13:37:35 +0300

    add-product-list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import data from "./data";
 
 function App() {
   return (
@@ -6,7 +7,25 @@ function App() {
       <header>
         <a href="/">Undercut</a>
       </header>
-      <main>List products</main>
+      <main>
+        <h1>List products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <img src={product.image} alt={product.name} />
+              <div className="product-info">
+                <a className="product-link" href={`/product/${product.slug}`}>
+                  <h2>{product.name}</h2>
+                </a>
+                <h5>
+                  <strong>${product.price}</strong>
+                </h5>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
