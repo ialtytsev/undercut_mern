fa584825886f9d5108d8fc3bc2ae75df5f7c02ba
ialtytsev <alty1998alty@gmail.com>
2022-08-05 11:08:06 +0300

    create frontend folder

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Undercut</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
