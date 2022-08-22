4fbe0da5e47ff1ef1c315722566ec7db4e413fd6
ialtytsev <alty1998alty@gmail.com>
2022-08-22 10:30:36 +0300

    complete sign-in and shipping address screens

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -4,14 +4,23 @@ import { createContext } from "react";
 export const Store = createContext();
 
 const initialState = {
+  userInfo: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null,
+
   cart: {
-    cartItems: [],
+    shippingAddress: localStorage.getItem("shippingAddress")
+      ? JSON.parse(localStorage.getItem("shippingAddress"))
+      : {},
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
   },
 };
 
 function reducer(state, action) {
   switch (action.type) {
-    case "CART_ADD_ITEM":
+    case "CART_ADD_ITEM": {
       const newItem = action.payload;
       const existItem = state.cart.cartItems.find(
         (item) => item._id === newItem._id
@@ -21,7 +30,7 @@ function reducer(state, action) {
             item._id === existItem._id ? newItem : item
           )
         : [...state.cart.cartItems, newItem];
-
+      localStorage.setItem("cartItems", JSON.stringify(cartItems));
       return {
         ...state,
         cart: {
@@ -29,6 +38,30 @@ function reducer(state, action) {
           cartItems,
         },
       };
+    }
+    case "CART_REMOVE_ITEM": {
+      const cartItems = state.cart.cartItems.filter(
+        (item) => item._id !== action.payload._id
+      );
+      localStorage.setItem("cartItems", JSON.stringify(cartItems));
+      return { ...state, cart: { ...state.cart, cartItems } };
+    }
+    case "USER_SIGNIN": {
+      return { ...state, userInfo: action.payload };
+    }
+    case "USER_SIGNOUT": {
+      return {
+        ...state,
+        userInfo: null,
+        cart: { cartItems: [], shippingAddress: {} },
+      };
+    }
+    case "SAVE_SHIPPING_ADDRESS": {
+      return {
+        ...state,
+        cart: { ...state.cart, shippingAddress: action.payload },
+      };
+    }
     default:
       return state;
   }
